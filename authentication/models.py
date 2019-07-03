3018edf8629331f2a3a00dcb53e835b78853c90f
Bruno Darshan <brunodarshan@gmail.com>
2019-07-03 11:40:24 -0300

    add fields to user model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import secrets
+import django.contrib.auth.models as adminModels
 
-# Create your models here.
+class User(adminModels.User):
+  # USER_STATUS_CHOICES = (
+  #   (0,'inactive'),
+  #   (1,'active'),
+  # )
+  bio = models.CharField(max_length=180, blank=True)
+  email_verified = models.DateField(auto_now=False, auto_now_add=False)
+  verification_code=models.CharField(max_length=128,blank=False, db_index=True, unique=True)
+
+  def save(self):
+    token = secrets.token_hex(20)
+    while len(User.objects.filter(verification_code=token)) > 0:
+      token = secrets.token_hex(20)
+    self.verification_code = token
+    super(User, self).save()
+    
