317b985b987befe4fd3f33ddbda4c5952050a41d
Bruno Darshan <brunodarshan@gmail.com>
2019-07-03 19:20:21 -0300

    rename models and refactor

--- profile/views.py
+++ profile/views.py
@@ -1,56 +1,47 @@
 from django.shortcuts import render
-from profile.models import Perfil, Convite
+from profile.models import Invite
 from django.shortcuts import redirect
 from authentication.models import User
 
 def index(request):
-    src = {'current_user': current_user(request)}
+    current_user = current_user(request)
+    src = {'current_user': current_user }
     return render(request, 'index.html', src )
 
 
-def exibir_perfil(request, perfil_id):
-    perfil = Perfil.objects.get(id=perfil_id)
-
-    return render(request, 'perfil.html',
-                  {'perfil': perfil,
-                   'perfil_logado': current_user(request), 'user_logado': get_user_logado(request)})
+def show(request, profile_id):
+    profile = User.objects.get(id=profile_id)
+    args = {'profile': profile, 'current_user': current_user(request)}
+    return render(request, 'profile.html', args)
 
 
-def convidar(request, perfil_id):
-    perfil_a_convidar = Perfil.objects.get(id=perfil_id)
-    print(perfil_a_convidar.id)
-    perfil_logado = current_user(request)
-    print(perfil_logado.id)
-    perfil_logado.convidar(perfil_a_convidar)
+def invite_user(request, profile_id):
+    recipient = User.objects.get(id=profile_id)
+    current_user(request).send_invite(recipient)
+    return redirect('index')
+
+def accept_invite(request, convite_id):
+    invite = Invite.objects.get(id=convite_id)
+    invite.accept()
+    invite.save()
     return redirect('index')
 
 
-def get_user_logado(request):
-    return 
-
-
-def current_user(request):
-    return request.user
-
-
-def aceitar(request, convite_id):
-    convite = Convite.objects.get(id=convite_id)
-    convite.aceitar()
-    return redirect('index')
-
-
-def exibir(request, perfil_id):
-    perfil = Perfil.objects.get(id=perfil_id)
-    perfil_logado = current_user(request)
-    ja_eh_contato = perfil in perfil_logado.contatos.all()
-    e_seu_perfil = perfil_logado.id == perfil_id
+def exibir(request, profile_id):
+    profile = User.objects.get(id=profile_id)
+    profile_logado = current_user(request)
+    ja_eh_contato = profile in profile_logado.contatos.all()
+    e_seu_profile = profile_logado.id == profile_id
     print(ja_eh_contato)
-    if not e_seu_perfil:
-        return render(request, 'perfil.html',
-                      {'perfil': perfil, 'perfil_logado': current_user(request),
+    if not e_seu_profile:
+        return render(request, 'profile.html',
+                      {'profile': profile, 'profile_logado': current_user(request),
                        'user_logado': get_user_logado(request),
                        'ja_eh_contato': ja_eh_contato})
     else:
-        return render(request, 'perfil.html',
-                      {'perfil': perfil, 'perfil_logado': current_user(request),
-                       'e_seu_perfil': e_seu_perfil})
+        return render(request, 'profile.html',
+                      {'profile': profile, 'profile_logado': current_user(request),
+                       'e_seu_profile': e_seu_profile})
+
+def current_user(request):
+    return request.user
