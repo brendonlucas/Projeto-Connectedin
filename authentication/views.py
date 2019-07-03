1cbb34e275ac8bd475ecf42e019a7db70d5f21d8
Bruno Darshan <brunodarshan@gmail.com>
2019-07-03 13:06:58 -0300

    override view of login(signin)

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+import django.contrib.auth.views as djangoAuthViews
+
+
+
 from django.shortcuts import *
 from django.utils.decorators import method_decorator
 from django.views.generic.base import View
@@ -19,14 +24,25 @@ class RegisterUserView(View):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             dados_form = form.cleaned_data
-            usuario = User.objects.create_user(username=dados_form['nome'], email=dados_form['email'],
-                                               password=dados_form['senha'], first_name=dados_form['first_name'],
-                                               last_name=dados_form['last_name'])
+            usuario = User.objects.create_user(username=dados_form['username'], 
+                                                email=dados_form['email'], 
+                                                password=dados_form['password'], 
+                                                first_name=dados_form['first_name'], 
+                                                last_name=dados_form['last_name'])
 
-            usuario_dados = Perfil(telefone=dados_form['telefone'], nome_empresa=dados_form['nome_empresa'],
-                                   user=usuario)
+            # usuario_dados = Perfil(telefone=dados_form['telefone'], nome_empresa=dados_form['nome_empresa'],
+            #                        user=usuario)
 
-            usuario_dados.save()
-            return redirect('login')
+            # usuario_dados.save()
+            return redirect('signin')
         return render(request, self.template_name, {'form': form})
 
+class LoginUserView(djangoAuthViews.LoginView):
+    template_name = 'login.html'
+    form = AuthenticationForm()
+
+    def get(self, request):
+        return render(request, self.template_name, {'form': form})
+    
+    def post(self, request):
+        return super(LoginUserView, self).post(request)
