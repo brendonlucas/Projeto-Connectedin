d34af775f91a174d5ef4d08f171f31a9cc12ff2b
Bruno Darshan <brunodarshan@gmail.com>
2019-07-03 20:51:27 -0300

    set email_verified as null

--- authentication/models.py
+++ authentication/models.py
@@ -9,15 +9,15 @@ class User(adminModels.User):
   #   (1,'active'),
   # )
   bio = models.CharField(max_length=180, blank=True)
-  email_verified = models.DateField(auto_now=False, auto_now_add=False)
+  email_verified = models.DateField(null=True, blank=True, auto_now=False, auto_now_add=False)
   verification_code=models.CharField(max_length=128,blank=False, db_index=True, unique=True)
 
-  def save(self):
+  def save(self, *args, **kwargs):
     token = secrets.token_hex(20)
     while len(User.objects.filter(verification_code=token)) > 0:
       token = secrets.token_hex(20)
     self.verification_code = token
-    super(User, self).save()
+    super(User, self).save(*args, **kwargs)
     
   def send_invite(self, recipient):
     user.invite.add(recipient=recipient)
