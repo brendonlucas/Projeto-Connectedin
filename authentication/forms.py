7dcb07733184177e4ab9998d4910d8b00c1f1c0d
Bruno Darshan <brunodarshan@gmail.com>
2019-07-03 11:43:30 -0300

    include input password widget

--- authentication/forms.py
+++ authentication/forms.py
@@ -7,7 +7,7 @@ class UserRegisterForm(forms.Form):
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
     email = forms.EmailField(required=True)
-    password = forms.CharField(required=True)
+    password = forms.CharField(required=True, widget=forms.PasswordInput)
     
 
     def is_valid(self):
