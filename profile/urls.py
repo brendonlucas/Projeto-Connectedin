0bacc23956c3f8ebc061be93f1da07ad7a2e7158
Bruno Darshan <brunodarshan@gmail.com>
2019-07-06 10:24:14 -0300

    rename routes profile

--- profile/urls.py
+++ profile/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from profile.views import show, invite_friendship, accept_friendship, refuse_friendship
 
 urlpatterns = [
-  path('/<int:profile_id>', show, name='show_user'),
-  path('/<int:profile_id>/invite', invite_friendship, name='invite_friendship'),
-  path('/invite/<int:invite_id>/accept', accept_friendship, name='accept_friendship'),
-  path('/invite/<int:invite_id>/refuse', refuse_friendship, name='refuse_friendship'),
+  path('<int:profile_id>', show, name='show_user'),
+  path('<int:profile_id>/invite', invite_friendship, name='invite_friendship'),
+  path('invite/<int:invite_id>/accept', accept_friendship, name='accept_friendship'),
+  path('invite/<int:invite_id>/refuse', refuse_friendship, name='refuse_friendship'),
 ]
