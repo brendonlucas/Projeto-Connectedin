a0c324bda5113353504a4d9341e1c93a5327f483
julianamarques <julianabmarques@live.com>
2019-07-14 17:49:10 -0300

    Add comments

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import add_post, delete_post
+from .views import add_post, delete_post, comment_post
 
 urlpatterns = [
     path('add/', add_post, name='add_post'),
     path('<int:post_id>/delete/', delete_post, name='delete_post'),
+    path('<int:post_id>/comment/', comment_post, name='comment_post'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -26,3 +26,20 @@ def delete_post(request, post_id):
     post.delete()
 
     return redirect('index')
+
+@login_required
+def comment_post(request, post_id):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        post = Post.objects.get(id=post_id)
+        
+        if form.is_valid():
+            data_form = form.cleaned_data
+            Post.objects.create(content=data_form['content'], user=request.user, type_post='1', commented_post=post)
+
+            return redirect('index')
+        else:
+            return redirect('index')               
+
+    else:
+        return redirect('index')
